0d5632290a8739f0b3ab6f3637ac01704579cf98
Faheemah Shaikh <Fashaikh3@s.sfusd.edu>
2018-06-22 13:24:05 -0700

    created python calculator
    example of calculator

---
+++ test.py
@@ -0,0 +1,19 @@
+#num1=2 # this is a comment
+#num2=7453453.2 # this is a decimal
+#text="Silence"
+
+#Outputs
+#print(num1+num2) # sum
+#print("Hello, world!") # TODO: Add another word
+#print("i want some lunch!")
+#print(text)
+
+num1 = 0
+num2 = 0
+
+num1 = (input(" Give us a number: "))
+num2 = (input("Give us another number: "))
+
+sum = num1 + num2
+
+print("Here is your sum: %d " %sum)
